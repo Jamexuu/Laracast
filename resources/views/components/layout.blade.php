0d391c31b537f703e167eabc9e4bb899d63f8e29
Jamexuu <jamesmercado2209@gmail.com>
2025-10-29 00:57:47 +0800

    create nav-link component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-300">
 
 <head>
     <meta charset="UTF-8">
@@ -9,17 +9,7 @@
     <title>Laravel Practice</title>
 </head>
 
-<body>
-    <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-900">
-  <body class="h-full">
-  ```
--->
+<body class="h-full">
     <div class="min-h-full">
         <nav class="bg-gray-800/50">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -31,13 +21,9 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                                <a href="/" aria-current="page"
-                                    class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                                <a href="/about"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                                <a href="/contact"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                                <x-nav-link href="/" :active="request()->is('/')" type="a">Home</x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')" type="a">About</x-nav-link>
+                                <x-nav-link :active="request()->is('contact')" type="button">Contact</x-nav-link>
                             </div>
                         </div>
                     </div>
@@ -94,7 +80,7 @@
                         class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
                     <a href="/about"
                         class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                    <a href="contact"
+                    <a href="/contact"
                         class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
                 </div>
                 <div class="border-t border-white/10 pt-4 pb-3">
