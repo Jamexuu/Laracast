1e1a0af9c27fdc017779d124fbeafac7811b4774
Jamexuu <jamesmercado2209@gmail.com>
2025-11-11 00:04:38 +0800

    learn simple Eloquent function in Laravel

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Software Developer',
-                'salary' => '80,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'College Professor',
-                'salary' => '50,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Director',
-                'salary' => '60,000'
-            ]
-        ];
-    }
-    
-    public static function find($id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+    protected $table = 'job_listings';
 
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
